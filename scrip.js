69ff37e2330b714d48faeee54d93416431d91493
evelynolofin <olofinevelyn@gmail.com>
2025-05-04 14:54:46 -0700

    my first commit

---
+++ scrip.js
@@ -0,0 +1,36 @@
+let myAge = 40
+
+console.log(myAge)
+
+let Evelyn = "my birthday is on the 31st of january"
+
+console.log(Evelyn)
+
+const student = {
+    name: "Evelyn",
+    age: 34,
+    color: "dark",
+    height: "5.4 ft",
+    isPregnant: true,
+};
+
+console.log(student)
+
+let a = 12;
+let b = 34;
+let c = 43;
+
+const sum = a+b+c;
+console.log(sum);
+
+const sub = c-b;
+console.log(sub);
+
+const div = 34/12;
+console.log(div)
+
+const mut = a*b;
+console.log(mut)
+
+const modul = c%b;
+console.log(modul)
